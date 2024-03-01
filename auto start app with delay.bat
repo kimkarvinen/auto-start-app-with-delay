16ae73e19fd04a12340fe48481bfead2fdf05d38
kimkarvinen <161712265+kimkarvinen@users.noreply.github.com>
2024-03-01 18:19:24 +0800

    Add files via upload

---
+++ auto start app with delay.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "D:\TSCERESSQL\DM_SFTPRobinsons_updated_TSCERES\"
+echo Please do not close this window
+echo Closing this window, Jimac will not be reliable for penalties.
+echo Application POS hookup is starting... Please wait.
+echo --Kim Karvinen
+timeout /t 20 /nobreak >nul
+start "" "DownloadManager.exe"
